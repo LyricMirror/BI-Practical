4bd9b5c95048d76c6b64c480deba9b870da46591
NirajSahu0412 <nirajsahu0412@gmail.com>
2024-01-18 19:10:25 +0530

    added Practical6/b.py

---
+++ Practical6/b.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+x = np.array([5, 15, 25, 35, 45, 55])
+y = np.array([5, 20, 14, 32, 22, 38])
+
+
+def lin_reg(x, y):
+    x = x.reshape(-1, 1)  # Reshape x to a 2D array
+    model = LinearRegression()
+    model.fit(x, y)
+    slope = model.coef_[0]
+    intercept = model.intercept_
+    return {"slope": slope, "intercept": intercept}
+
+
+coefficient = lin_reg(x, y)
+
+
+def predict(x, coefficient):
+    return coefficient["slope"] * x + coefficient["intercept"]
+
+
+plt.scatter(x, y, marker='o', color='teal')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Linear Regression with scikit-learn')
+plt.plot(x, predict(x, coefficient), color='red')
+plt.show()
+
+new_x = np.array([6])
+predicted_y = predict(new_x, coefficient)
+print("Predicted y value for new x: ", predicted_y)
